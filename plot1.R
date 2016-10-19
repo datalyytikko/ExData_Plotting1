266cf8ad1e074185e5446d26f3a84e34bf43014b
Datalyytikko <datalyytikko@fastmail.com>
2016-10-19 07:37:29 +0300

    Fixes.

--- plot1.R
+++ plot1.R
@@ -1,10 +1,34 @@
+library(readr)
+library(dplyr)
+
+#load data if needed
 if(!file.exists("data.zip")) {
     url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
     download.file(url, "data.zip")    
 }
 
+#unpack data if needed
 if(!file.exists("household_power_consumption.txt")) {
     unzip(zipfile = "data.zip")
 }
 
+#read all
+data <- read_delim(file = "household_power_consumption.txt", 
+                   n_max = 2100000, 
+                   delim =";", 
+                   na = c("?"))
 
+#leave only the ones we're interested in
+data <- data %>%
+    filter(Date == "2/2/2007" | Date == "1/2/2007")
+
+#fix datatypes
+data$Date2 <- as.Date(data$Date, format="%d/%m/%Y")
+
+#create plot and save it as png
+png("plot1.png", width = 480, height = 480)
+    hist(data$Global_active_power, 
+         col = "red", 
+         xlab ="Global Active Power (kilowatts)", 
+         main="Global Active Power")
+dev.off()
