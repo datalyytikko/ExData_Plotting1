9d59dfffa870a35d7c79c66dd1a6db7246010361
Datalyytikko <datalyytikko@fastmail.com>
2016-10-19 08:13:56 +0300

    plot 2 added.

---
+++ plot2.R
@@ -0,0 +1,38 @@
+#Source code for creating plot2
+library(lubridate)
+library(readr)
+library(dplyr)
+
+#load data if needed
+if(!file.exists("data.zip")) {
+    url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    download.file(url, "data.zip")    
+}
+
+#unpack data if needed
+if(!file.exists("household_power_consumption.txt")) {
+    unzip(zipfile = "data.zip")
+}
+
+#read all
+data <- read_delim(file = "household_power_consumption.txt", 
+                   n_max = 2100000, 
+                   delim =";", 
+                   na = c("?"))
+
+#leave only the ones we're interested in
+data <- data %>%
+    filter(Date == "2/2/2007" | Date == "1/2/2007") 
+
+#fix datatypes
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+data$datetime = data$Date + seconds(data$Time)
+
+#create plot and save it as png
+png("plot2.png", width = 480, height = 480)
+    plot(x = data$datetime, 
+         y = data$Global_active_power, 
+         type="l",
+         ylab="Global Active Power (kilowatts)",
+         xlab="")
+dev.off()
